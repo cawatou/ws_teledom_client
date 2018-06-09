a72053ed7f43a677d9b9fece7f5a194191fc7ee0
cawatou <cawatou@gmail.com>
2018-06-09 18:53:00 +0300

    history, news, orders, calls, pass, open

--- index.js
+++ index.js
@@ -1,47 +1,33 @@
 var ws = io('http://185.22.235.182:8111');
-
-ws.on('candidate', function(data) {
-	console.log(data.ice);
-	// добавляем пришедший ICE-кандидат
-	if(data.ice && pc.remoteDescription) pc.addIceCandidate(new RTCIceCandidate(data.ice));
-});
-
-ws.on('offer', function(sdp) {
-	console.log('Пришел Offer от другого участника', sdp);
-	if(!pc) {
-		// Пришел Offer от другого участника
-		createPC(false);
-	}
-	setRemoteSDP(sdp);
-});
+var data;
 
 ws.on("answer", function(data){
 	console.log(data);	
 })
 
 function init(){
-	var data = {
+	 data = {
 		serial_number: '84005035c504382e09ce'
 	};
 	ws.emit('init', data);
 }
 
 function config(){
-	var data = {
+	data = {
 		serial_number: '84005035c504382e09ce'
 	};
 	ws.emit('config', data);
 }
 
 function key_open(){
-	var data = {
+	data = {
 		key_number: '70565AD129BA'
 	};
 	ws.emit('key_open', data);
 }
 
 function getcode(){
-	var data = {
+	data = {
 		phone: '79046153341'
 	};
 	ws.emit('mobile_getcode', data);
@@ -49,7 +35,7 @@ function getcode(){
 
 function checkcode(){
 	var code = document.getElementById('code').value;
-	var data = {
+	data = {
 		phone: '79046153341',
 		code: code
 	};
@@ -59,4 +45,33 @@ function checkcode(){
 function auth(){
 	ws.emit('mobile_auth');
 }
+
+function history(){
+	data = {
+		from: '',
+		to: ''
+	};
+	ws.emit('mobile_history', data);
+}
+
+function news(){
+	ws.emit('mobile_news', data);
+}
+
+function orders(){
+	ws.emit('mobile_orders', data);
+}
+
+function calls(){
+	ws.emit('mobile_calls', data);
+}
+
+function pass(){
+	ws.emit('mobile_pass', data);
+}
+
+function open(){
+	ws.emit('mobile_opendoor', data);
+}
+
 
