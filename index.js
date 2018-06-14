a6d87f77f9f99c9a2df34c7f2a89e7d5ab543e86
cawatou <cawatou@gmail.com>
2018-06-14 20:05:47 +0300

    exclude answer

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-var ws = io('http://185.22.235.182:8111');
+var ws = io.connect('http://185.22.235.182:8111');
 var data;
 
-ws.on("answer", function (data) {
-    console.log(data);
-})
-
 function init() {
-    data = {
-        serial_number: '84005035c504382e09ce',
-        token: 'Gs1codN2NLFFxQMF-lD_yNU-rfkI70XH'
-    };
-    ws.emit('init', data);
+    data = {serial_number: '84005035c504382e09ce'};
+    ws.emit('init', data, (res) => console.log(res));
 }
 
 function config() {
     data = {
-        serial_number: '84005035c504382e09ce'
+        token: 'Gs1codN2NLFFxQMF-lD_yNU-rfkI70XH',
+        sign: sign
     };
     ws.emit('config', data);
 }
@@ -75,4 +69,25 @@ function opendoor() {
     ws.emit('mobile_opendoor', data);
 }
 
+/*
+//var code = document.getElementById('code').value;
+var code = '188065';
 
+var tail = code.split('');
+tail = tail.map((item) => Number(item));
+
+var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+key = key.concat(tail);
+var iv = [ 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ];
+iv = iv.concat(tail);
+
+// Convert text to bytes
+var text = '84005035c504382e09ce';
+var textBytes = aesjs.utils.utf8.toBytes(text);
+
+var aesOfb = new aesjs.ModeOfOperation.ofb(key, iv);
+var encryptedBytes = aesOfb.encrypt(textBytes);
+
+// To print or store the binary data, you may convert it to hex
+var sign = aesjs.utils.hex.fromBytes(encryptedBytes);
+console.log(sign);*/
